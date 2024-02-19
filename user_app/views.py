ac94cb2d7c35a221b12ad8188f4c119f8bdd5928
sommy-josh <chisomzzy1@>
2024-02-19 12:40:13 +0100

    added some function based views

--- user_app/views.py
+++ user_app/views.py
@@ -11,8 +11,34 @@ from rest_framework.permissions import IsAuthenticated
 from .tokens import create_jwt_pair_for_user
 
 
+
+# This is how to create a post with function based views
+
+# @api_view(['POST'])
+# def create_post(request):
+#     serializer = PostSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+''''''''''''''''''''''''
+
+# @api_view(['GET'])
+# def list_posts(request):
+#     posts = Post.objects.all()
+#     serializer = PostSerializer(posts, many=True)
+#     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def get_post(request, pk):
+#     try:
+#         post = Post.objects.get(pk=pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#     except Post.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
     
-    
+''''''''''''''''''''''''    
 
 @api_view(['POST'])
 def login(request):
